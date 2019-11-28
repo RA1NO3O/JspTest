9b6769a81a121dd3ce0e52b4e13a74dce54c89ee
RA1N <RA1NN1AR@users.noreply.github.com>
2019-11-28 11:52:25 +0800

    Nov28th

---
+++ Dweb/src/cn/itcast/servlet/TestServlet01.java
@@ -0,0 +1,42 @@
+package cn.itcast.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.jasper.tagplugins.jstl.core.Out;
+
+/**
+ * Servlet implementation class TestServlet01
+ */
+public class TestServlet01 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=utf-8");
+//		response.setHeader("Content-Type", "text/html;charset=utf-8");
+		response.getWriter().print("软件1811");
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		doGet(request, response);
+//		PrintWriter out = response.getWriter();
+//		out.print("36 zy");
+		
+	}
+
+}
